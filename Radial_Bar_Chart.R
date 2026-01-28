80d5f84237211c88316420094aa1529c695a4a02
Chen Huang <hcheni@whu.edu.cn>
2026-01-29 02:03:58 +0800

    Refactor Radial Bar Chart code for adding scatter plots with error bars

--- Radial_Bar_Chart.R
+++ Radial_Bar_Chart.R
@@ -1,9 +1,17 @@
 setwd("Path_to/Data.txt")
 library(tidyverse)
 library(ggplot2)
-data <- read.table("Data.txt",sep = "\t",header = TRUE)
-head(data)
-level <- data[,1]
-level <- rev(level)
-data$NAME <- factor(data$NAME,level=level)
-ggplot(data, aes(x = NAME,y=ALL))+geom_col(aes(y = ALL), fill = "#91A8D0")+geom_col(aes(y = ATG), fill = "#F7CAC9")+scale_y_continuous(limits = c(-9, 9))+coord_polar(start = 2.82,direction=-1)+theme_minimal() 
+data_main<- read.table("Data.txt",sep = "\t",header = TRUE)
+data_points<-read.table("Data.txt",sep="\t",header=TRUE)
+data_points$NAME<-gsub("","_",data_points$NAME)
+species_to_show<-data_points%>%
+group_by(NAME)%>%
+tally()%>%
+filter(n>1)%>%
+pull(NAME)
+data_points_filtered<-data_points%>%
+filter(NAME%in%species_to_show)
+level<-rev(data_main$Name)
+data_main$Name<-factor(data_main$Name,levels=level)
+data_points_filtered$NAME<-factor(data_points_filtered$NAME,levels=level)
+ggplot()+geom_col(data=data_main,aes(x=Name,y=ATG),fill="#cccccc")+geom_errorbar(data=data_main,aes(x=Name,ymin=ATG-SEM,ymax=ATG+SEM),width=0.4)+geom_jitter(data=data_points_filtered,aes(x=NAME,y=REL),shape=21,fill="#cccccc",color="black",size=0.3,stroke=0.1,alpha=1,width=0.2,height=0)+scale_y_continuous(limits=c(-5,6))+coord_polar(start=-0.1*pi,clip="off")+theme_minimal()
